aa7d4193a9c7a369e407ac4548dd3396e1e11136
Pablo Campuzano <60987878+Campuchan@users.noreply.github.com>
2023-05-17 10:34:34 +0200

    Delete pruebagamer directory

--- pruebagamer/procesador.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-// process the form data and generate a response
-$response = "Hola, $nombre! Tu email es $email.";
-echo $response;
-?>
